b0338bcb39b3b7469b316be1b0330d605135ecbc
Maciek Pytel <maciej.pytel@codilime.com>
2015-03-15 12:44:42 +0100

    Added some more complex tests for hooking monitor into class.
    Some tests are failing (and skipped) due to a known issue.
    More tests to follow.

--- object_monitor/core/tests/test_hook.py
+++ object_monitor/core/tests/test_hook.py
@@ -4,6 +4,11 @@ from object_monitor.core import hook
 from mock_monitor import MockMonitor
 
 
+def dummy_init(self):
+    ''' A dummy function used as __init__ in test classes. '''
+    self.param = 42
+
+
 class HookTest(unittest.TestCase):
     def tearDown(self):
         MockMonitor.reset()
@@ -22,23 +27,62 @@ class HookTest(unittest.TestCase):
         self.assertEqual(MockMonitor.get_state(test_cls, id2),
                          MockMonitor.STATE_INITIALISED)
 
+    def check_unmonitored(self, test_cls):
+        self.assertFalse(MockMonitor.is_monitored(test_cls))
+        self.assertFalse(hasattr(test_cls, '_monitor_counter'))
+        instance = test_cls()
+        self.assertFalse(hasattr(instance, '_monitor_id'))
+        self.assertEquals(MockMonitor.get_states(test_cls), {})
+
     def test_hook_empty_class(self):
         cls = type('SampleClass', (object,), {})
         cls = hook.monitor(MockMonitor)(cls)
         self.check_monitored(cls)
 
-    def test_hook_with_simple_baseclass(self):
+    def test_hook_simple_baseclass(self):
         base = type('SampleBase', (object,), {})
         cls = type('SampleClass', (base,), {})
         cls = hook.monitor(MockMonitor)(cls)
+        self.check_monitored(cls)
+        self.check_unmonitored(base)
 
+    @unittest.skip('This is a known issue with monitor decorator')
+    def test_hook_simple_derived(self):
+        cls = type('SampleClass', (object,), {})
+        cls = hook.monitor(MockMonitor)(cls)
+        derived = type('SampleDerived', (cls,), {})
+        self.check_monitored(cls)
+        self.check_unmonitored(derived)
+
+    def test_hook_init_in_baseclass(self):
+        base = type('SampleBase', (object,), {'__init__': dummy_init})
+        cls = type('SampleClass', (base,), {})
+        cls = hook.monitor(MockMonitor)(cls)
+
+        # make sure we didn't break __init__ in baseclass
+        base_instance = base()
+        self.assertEquals(base_instance.param, 42)
+        self.check_unmonitored(base)
+
+        # make sure __init__ from baseclass gets executed and monitoring works
+        instance = cls()
+        self.assertEquals(instance.param, 42)
+        instance_id = instance._monitor_id
+        self.assertEqual(MockMonitor.get_state(cls, instance_id),
+                         MockMonitor.STATE_INITIALISED)
+        instance = None
+        self.assertEqual(MockMonitor.get_state(cls, instance_id),
+                         MockMonitor.STATE_DESTROYED)
+
+    @unittest.skip('This is a known issue with monitor decorator')
+    def test_hook_init_usedby_derived(self):
+        cls = type('SampleClass', (object,), {'__init__': dummy_init})
+        cls = hook.monitor(MockMonitor)(cls)
+        derived = type('SampleDerived', (cls,), {})
         self.check_monitored(cls)
 
-        # make sure we haven't decorated base class
-        self.assertFalse(MockMonitor.is_monitored(base))
-        self.assertFalse(hasattr(base, '_monitor_counter'))
-        base_instance = base()
-        self.assertFalse(hasattr(base_instance, '_monitor_id'))
-        self.assertEquals(MockMonitor.get_states(base), {})
-
-
+        # make sure derived class is not monitored,
+        # but was properly initialised
+        self.check_unmonitored(derived)
+        derived_instance = derived()
+        self.assertEqual(derived_instance.param, 42)
